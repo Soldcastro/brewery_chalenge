9a9e97fca7896250d099af51204779ec5f9472eb
Soldcastro <solano.castro@outlook.com>
2025-06-08 11:14:27 -0300

    refactor airflow p/ usar postgres como db

--- dags/etl_brewery.py
+++ dags/etl_brewery.py
@@ -1,35 +1,34 @@
 from airflow import DAG
-from airflow.operators.bash import BashOperator
+from airflow.providers.standard.operators.bash import BashOperator
 from datetime import datetime, timedelta
 
 default_args = {
     'start_date': datetime(2024, 1, 1),
     'retries': 1,
-    'retry_delay': timedelta(minutes=1),
-    'email': ['solano.d.castro@gmail.com'],
-    'email_on_failure': True,
-    'email_on_retry': False
+    'retry_delay': timedelta(minutes=1)
 }
 
-with DAG('etl_brewery',
-         description='ETL process for brewery data',
-         schedule_interval='@daily',
-         default_args=default_args,
-         catchup=False) as dag:
+with DAG(
+    dag_id='etl_brewery',
+    description='ETL process for brewery data',
+    schedule=None,
+    default_args=default_args,
+    catchup=False
+    ) as dag:
 
     bronze_process = BashOperator(
         task_id='bronze_process',
-        bash_command='docker exec bronze-process python3 /app/scripts/process_bronze.py'
+        bash_command='docker exec spark python3 /app/scripts/process_bronze.py'
         )
     
     silver_process = BashOperator(
         task_id='silver_process',
-        bash_command='docker exec silver-process python3 /app/scripts/process_silver.py'
+        bash_command='docker exec spark python3 /app/scripts/process_silver.py'
         )
     
     gold_process = BashOperator(
         task_id='gold_process',
-        bash_command='docker exec gold-process python3 /app/scripts/process_gold.py'
+        bash_command='docker exec spark python3 /app/scripts/process_gold.py'
         )
 
     bronze_process >> silver_process >> gold_process
