42f37c889285a98c6e289381ba8264226d8f2b8e
Soldcastro <solano.castro@outlook.com>
2025-06-07 21:51:01 -0300

    acerto docker-compose

---
+++ scripts/process_gold.py
@@ -0,0 +1,68 @@
+"""
+# Brewery Challenge - Process Gold Layer
+"""
+
+# import necessary libraries
+from pyspark.sql import SparkSession, DataFrame
+
+# create a Spark session
+spark = SparkSession.builder \
+    .appName("Process Gold") \
+    .getOrCreate()
+
+def get_silver_data(read_path: str) -> DataFrame:
+    """
+    Function to read silver data from a given path.
+    """
+    try:
+        silver_df = spark.read.parquet(read_path)
+    except Exception as e:
+        print(f"Error reading Parquet file: {e}")
+        raise
+    if silver_df.isEmpty():
+        print("No data found in the silver layer.")
+        spark.stop()
+        raise ValueError("Empty DataFrame: No data to process.")
+    return silver_df
+
+def process_gold(silver_df: DataFrame) -> DataFrame:
+    """
+    Function to process silver data and write it to the gold layer.
+    """
+    try:
+        # Makes aggregation to count breweries by country and type
+        gold_df = silver_df.groupBy("country", "brewery_type") \
+            .count() \
+            .withColumnRenamed("count", "brewery_count")
+    except Exception as e:
+        print(f"Error during aggregation: {e}")
+        raise
+    return gold_df
+
+def write_gold_data(gold_df: DataFrame, write_path: str) -> None:
+    """
+    Function to write the processed gold data to a given path.
+    """
+    try:
+        gold_df.write.mode("overwrite").parquet(write_path)
+        print(f"Gold data written successfully to {write_path}")
+    except Exception as e:
+        print(f"Error writing Parquet file: {e}")
+        raise
+
+def main():
+    """
+    Main function to orchestrate the reading, processing, and writing of data.
+    """
+    read_path = "/datalake/silver/breweries"
+    write_path = "/datalake/gold/aggregated_breweries"
+
+    silver_df = get_silver_data(read_path)
+    gold_df = process_gold(silver_df)
+    write_gold_data(gold_df, write_path)
+
+    spark.stop()
+
+if __name__ == "__main__":
+    main()
+    
---
+++ scripts/process_silver.py
@@ -0,0 +1,80 @@
+"""
+# Brewery Challenge - Process Silver Layer
+"""
+# Import necessary libraries
+from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql.functions import col
+
+spark = SparkSession.builder \
+    .appName("Process Silver") \
+    .getOrCreate()
+
+# Defines the functions to process the silver data
+def get_bronze_data(spark_ctx, read_path) -> DataFrame:
+    """
+    Reads the bronze data from the specified path.
+    """
+    try:
+        bronze_df = spark_ctx.read.json(read_path, multiLine=True)
+    except IOError as e:
+        print(f"Error reading JSON file: {e}")
+        raise
+
+    # Check if the DataFrame is empty  
+    if bronze_df.isEmpty():
+        print("No data found in the bronze layer.")
+        spark_ctx.stop()
+        raise ValueError("Empty DataFrame: No data to process.")
+    return bronze_df
+
+def transform_bronze_to_silver(bronze_df) -> DataFrame:
+    """
+    Transforms the bronze DataFrame to the silver format.
+    
+    :param bronze_df: DataFrame containing the bronze data
+    :return: Transformed DataFrame in silver format
+    """
+    return bronze_df.select(
+        col("id").alias("brewery_id"),
+        col("name").alias("brewery_name"),
+        "brewery_type",
+        "city",
+        "state",
+        "country"
+    )
+
+def write_silver_data(silver_df, write_path) -> None:
+    """
+    Writes the silver DataFrame to the specified path.
+    
+    :param silver_df: DataFrame containing the silver data
+    :param write_path: Path to write the silver data
+    """
+    try:
+        silver_df.write.mode("overwrite").partitionBy('country', 'brewery_type').parquet(write_path)
+    except IOError as e:
+        print(f"Error writing DataFrame to silver layer: {e}")
+
+def main():
+    """
+    Main function to process the silver layer of the brewery data.
+    It reads the bronze data, transforms it to silver format, and writes it to the specified path.
+    """
+    read_path = "/datalake/bronze/raw_breweries.json"
+    write_path = "/datalake/silver/breweries"
+
+    # Read bronze data
+    bronze_df = get_bronze_data(spark, read_path)
+
+    # Transform to silver format
+    silver_df = transform_bronze_to_silver(bronze_df)
+
+    # Write silver data
+    write_silver_data(silver_df, write_path)
+
+    spark.stop()
+
+    print("Silver data processing completed successfully.")
+
+if __name__ == "__main__":
+    main()
